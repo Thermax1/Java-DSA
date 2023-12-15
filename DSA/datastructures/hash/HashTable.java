0ce1379bf18127183ff5ba2de1edd6ec712ef026
Thermax1 <a.d.munoz23@gmail.com>
2023-12-15 09:18:29 +0100

    work

--- DSA/datastructures/hash/HashTable.java
+++ DSA/datastructures/hash/HashTable.java
@@ -1,5 +1,8 @@
 package datastructures.hash;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class HashTable {
     private int size = 7;
     private Node[] dataMap;
@@ -21,12 +24,13 @@ public class HashTable {
 
     public void printTable() {
         for(int i = 0; i < dataMap.length;i++) {
-            System.out.println(i + ":");
+            System.out.print(i + ":");
             Node temp = dataMap[i];
             while(temp != null) {
-                System.out.println(" {" + temp.key + "= " + temp.value + "}");
+                System.out.print(" {" + temp.key + "=" + temp.value + "}");
                 temp = temp.next;
             }
+            System.out.println();
         }
     }
 
@@ -38,5 +42,41 @@ public class HashTable {
             hash = (hash + asciiValue * 23) % dataMap.length; //23 is a prime number, leading to more reandom number. We use modulo (7) so it will be from 0 to 6, allowing us to always access our address space
         }
         return hash;
+    }
+
+    public void set(String key, int value) { // O(1)
+        int index = hash(key);
+        Node newNode = new Node(key, value);
+        if(dataMap[index] == null) { //current index is empty
+            dataMap[index] = newNode; //this Node now points to our newNode at 6.
+        } else {
+            Node temp = dataMap[index]; //each dataMap[index] is its own LinkedList
+            while(temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode; // gets added to dataMap at correct spot
+        }
+    }
+
+    public int get(String key) { // O(1)
+        int index = hash(key);
+        Node temp = dataMap[index];
+        while(temp != null) {
+            if(temp.key == key) return temp.value;
+            temp = temp.next;
+        }
+        return 0;
+    }
+
+    public ArrayList keys() {
+        ArrayList<String> allKeys = new ArrayList<>();
+        for(int i = 0; i < dataMap.length;i++) {
+            Node temp = dataMap[i];
+            while(temp != null) {
+                allKeys.add(temp.key);
+                temp = temp.next;
+            }
+        }
+        return allKeys;
     }
 }
--- DSA/datastructures/hash/Main.java
+++ DSA/datastructures/hash/Main.java
@@ -4,7 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         HashTable myHash = new HashTable();
-        myHash.printTable();
+
+        myHash.set("nails",100);
+        myHash.set("tile",50);
+        myHash.set("lumber",80);
+
+        myHash.set("bolts",200);
+        myHash.set("screws",200);
+
+        myHash.set("glasses", 20);
+
+//        System.out.println(myHash.get("bolts"));
+//        System.out.println(myHash.get("tiles")); //will return 0
+
+        System.out.println(myHash.keys());
+
+
+
     }
 
 }
