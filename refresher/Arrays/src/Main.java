854411b9a0314393d47cd373a17d5b1eb4b5102f
Thermax1 <a.d.munoz23@gmail.com>
2024-01-11 17:13:37 +0100

    add

--- refresher/Arrays/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        //arrays: used to store multiple values in a single variable
-
-        String[] cars = {"Camaro","Toyota","Honda"};
-
-    }
-}
